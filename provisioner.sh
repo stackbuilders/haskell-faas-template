545bb9f20a14cceff753228ad3be6130f790acc1
Luis Corales <lcorales@stackbuilders.com>
2022-06-02 12:05:45 -0500

    Group commands in functions

--- provisioner.sh
+++ provisioner.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
 
-# Step 1. Install openfaas-cli
-curl -sSL https://cli.openfaas.com | sudo -E sh
+set -e
 
-# Step 2. Install microk8s and configure it
-# This step will add all the necessary packages
-# And permissions to use microk8s as needed
-sudo snap install microk8s --classic
-sudo usermod -a -G microk8s vagrant
-sudo chown -f -R vagrant ~/.kube
-newgrp microk8s
-microk8s status --wait-ready
-microk8s enable dns dashboard storage registry
-microk8s start
+function install_microk8s () {
+  # Install microk8s
+  sudo snap install microk8s --classic
 
-# Step 3. Clone OpenFaaS repo in order to use it
-git clone https://github.com/openfaas/faas-netes
-cd faas-netes
+  # Start the service with all modules enabled
+  microk8s status --wait-ready
+  microk8s enable dns dashboard storage registry
+  microk8s start
+}
 
-# Step 4. Create the OpenFaaS namespaces for the functions
-microk8s kubectl apply -f ./namespaces.yml
-microk8s kubectl apply -f ./yaml
+function microk8s_permissions () {
+  # Add all the necessary packages and permissions to use microk8s
+  sudo usermod -a -G microk8s vagrant
+  sudo chown -f -R vagrant ~/.kube
+  newgrp microk8s
+}
 
-# Step 5. Create a local Docker container registry 
-docker run -d -p 5000:5000  --restart=always --name registry registry:2
+function install_openfaas () {
+  # Install openfaas-cli
+  curl -sSL https://cli.openfaas.com | sudo -E sh
+}
+
+function clone_faasnetes () {
+  # Clone the faasnetes git repo in order to create the functions
+  git clone https://github.com/openfaas/faas-netes
+  cd faas-netes
+}
+
+function openfaas_namespaces () {
+  # Create the OpenFaaS namespaces for the functions
+  microk8s kubectl apply -f ./namespaces.yml
+  microk8s kubectl apply -f ./yaml
+}
+
+function create_registry () {
+  # Create local docker registry
+  docker run -d -p 5000:5000  --restart=always --name registry registry:2
+}
+
+install_microk8s
+microk8s_permissions
+install_openfaas
+clone_faasnetes
+openfaas_namespaces
+create_registry
